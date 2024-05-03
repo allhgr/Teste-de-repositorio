2e561cfec2aaf502454dcd157cb744cb4961feea
Neggs <henriqueryan011gmail.com>
2024-05-02 21:10:00 -0300

    Alteração de escolhas na calculadora

--- calculadora.py
+++ calculadora.py
@@ -1,7 +1,16 @@
-print("Calculadora de Soma Simples")
+print("Calculadora de Soma e Subtração Simples")
+opcao = int(input("1 - Soma \n2 - Subtração \nEscolha uma opção: "))
 
-a = int(input("Digite o primeiro numero para soma: "))
-b = int(input("Digite o segundo numero para soma: "))
-resultado = a + b
+if opcao == 1:
+    print("Calculadora de Soma Simples")
+    a = int(input("Digite o primeiro numero para soma: "))
+    b = int(input("Digite o segundo numero para soma: "))
+    resultado = a + b
+    print(f"Resultado de sua soma:\n {resultado}")
 
-print(f"Resultado de sua soma:\n {resultado}")
+if opcao == 2:
+    print("Calculador de Subtração Simples")
+    a1 = int(input("Digite o primeiro numero para soma: "))
+    b1 = int(input("Digite o segundo numero para soma: "))
+    resultado1 = a1 + b1
+    print(f"Resultado de sua soma:\n {resultado1}")
