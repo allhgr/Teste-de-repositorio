5d47ae3c635bade5d56d01b62a5db31c84a4d838
Allison <allison.htompson@gmail.com>
2024-05-02 21:03:49 -0300

    Commit de calculador simples

---
+++ calculadora.py
@@ -0,0 +1,7 @@
+print("Calculadora de Soma Simples")
+
+a = int(input("Digite o primeiro numero para soma: "))
+b = int(input("Digite o segundo numero para soma: "))
+resultado = a + b
+
+print(f"Resultado de sua soma:\n {resultado}")
